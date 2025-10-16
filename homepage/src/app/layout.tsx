63b12b7c6f1c820b71073ad31e3700ddac91be36
Alan <yeungbluecat123@gmail.com>
2025-10-15 22:12:07 -0700

    a

--- homepage/src/app/layout.tsx
+++ homepage/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
 
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>

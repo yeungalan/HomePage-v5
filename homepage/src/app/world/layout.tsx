0013143c8f87a2045ce2dbec1d956d34cacb355e
Alan Yeung <yeunghy@amazon.com>
2026-03-25 19:56:53 +0000

    fix: set iPhone status bar to black on /world page

--- homepage/src/app/world/layout.tsx
+++ homepage/src/app/world/layout.tsx
@@ -1,7 +1,13 @@
-export default function TestLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <>{children}</>
+import type { Metadata, Viewport } from 'next';
+
+export const metadata: Metadata = {
+  title: 'World Map',
+};
+
+export const viewport: Viewport = {
+  themeColor: '#000000',
+};
+
+export default function WorldLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
